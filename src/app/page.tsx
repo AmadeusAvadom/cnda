cf582c38352ada03bb8d5aea2cc8918da6c3a813
Amadeus <58746126+AmadeusAvadom@users.noreply.github.com>
2024-06-24 15:10:17 -0500

    Cambios del 24-06-2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,34 @@
+'use client'
+
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+
 const Navbar= () => {
   return (
+    <>
     <div>
-      <h3 className="text-xl bg-white p-2 text-center">Comision Nacional de Derechos Humanos</h3>
+      <h3 className="text-[24px] bg-white p-2 text-center font-medium">Comision Nacional de Derechos Humanos</h3>
+      <div className="cnd--main text-center">
+        <div className="flex-col flex font-bold">
+          <span className="text-[30px] text-white">DEFENSA JUDICIALES</span>
+          <span className="text-[30px] text-[#fde047]">EN DERECHO PENAL</span>
+        </div>
+        <div className="flex-col flex">
+          <span className="text-[25px] text-[#fde047]">Ejercemos tu representación judicial en</span>
+          <span className="text-[25px] text-white">Delitos sexuales</span>
+          <span className="text-[25px] text-white">Estupefacientes y narcotrafico</span>
+          <span className="text-[25px] text-white">Homicidio y feminicidio </span>
+          <span className="text-[25px] text-white">Violencia intrafamiliar</span>
+          <span className="text-[25px] text-white">Concierto para delinquir</span>
+          <span className="text-[25px] text-white">Falsedad en documentos y monedas</span>
+          <span className="text-[25px] text-white">Hurto, estafa y extorsión</span>
+          <span className="text-[25px] text-white">Lavado de activos y testaferrato</span>
+          <span className="text-[25px] text-white">Delitos informaticos</span>
+          <span className="text-[25px] text-white">Delitos de contratación estatal</span>
+        </div>
+      </div>
     </div>
+    <FloatingWhatsApp phoneNumber='+573170386862' accountName='' chatMessage="Hola ¿en que podriamos ayudarle?"/>
+    </>
   )
 }
 
